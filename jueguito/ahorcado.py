07514f425f5f6253b617addba1ee2b791234f0a4
Eli <elizabethtrinitygs@gmail.com>
2021-06-21 21:30:35 -0500

    save

---
+++ jueguito/ahorcado.py
@@ -0,0 +1,13 @@
+#Crear documento con lista de palabras
+#Leer el documento e imprimirlo en consola
+#imprimir una palabra aleatoriamente
+
+#.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+
+
+
+def game():
+    pass
+
+if __name__ == '__main__':
+    game()
