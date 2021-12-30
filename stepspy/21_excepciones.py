267a19a772fef5826bdfac160eb5d68ceb3eb39a
Eli <elizabethtrinitygs@gmail.com>
2021-12-29 22:15:01 -0600

    ejemplo de excepciones de control de flujo

--- stepspy/21_excepciones.py
+++ stepspy/21_excepciones.py
@@ -28,18 +28,18 @@ Traceback (most recent call last):
 ZeroDivisionError: division by zero
  '''
 
-def divide_elementos_de_lista(lista, divisor):
-    #programacion defensiva
-    try:
-        return [i / divisor for i in lista]
-        #si alguien usa el 0 para dividir, regresa la lista
-    except ZeroDivisionError as e:
-        print(e)
-        return lista
+# def divide_elementos_de_lista(lista, divisor):
+#     #programacion defensiva
+#     try:
+#         return [i / divisor for i in lista]
+#         #si alguien usa el 0 para dividir, regresa la lista
+#     except ZeroDivisionError as e:
+#         print(e)
+#         return lista
 
-lista = list(range(10))
-divisor = 0
-print(divide_elementos_de_lista(lista, divisor))
+# lista = list(range(10))
+# divisor = 0
+# print(divide_elementos_de_lista(lista, divisor))
 ''' // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  '''
 ''' :::::Cambiando divisor de 0 a 3::::::
     [0.0, 0.3333333333333333, 0.6666666666666666, 1.0, 1.3333333333333333, 1.6666666666666667, 2.0, 2.3333333333333335, 2.6666666666666665, 3.0]
@@ -47,3 +47,23 @@ print(divide_elementos_de_lista(lista, divisor))
     print(e)--> division by zero
                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
  '''
+
+#EXCEPCIONES COMO CONTROL DE FLUJO
+def busca_pais(paises, pais):
+    '''
+    Paises es un diccionario. Pais es la llave.
+    Codigo con el principio EAFP.
+    '''    
+    try:
+        return paises[pais]
+    except KeyError:
+        #return None
+        return print('\n \nTu búsqueda no se encuentra en la base de datos')
+
+paises = {
+        'Colombia': 'Bogota',
+        'Uruguay': 'Montevideo',
+}
+pais = 'Mexico'
+print(busca_pais(paises, pais))
+
