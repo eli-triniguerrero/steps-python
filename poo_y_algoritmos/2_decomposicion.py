07429fcfbbf7772514214a78d091d90ceb6e1e9a
Eli <elizabethtrinitygs@gmail.com>
2022-01-18 21:12:04 -0600

    decomposicion

---
+++ poo_y_algoritmos/2_decomposicion.py
@@ -0,0 +1,30 @@
+#DECOMPOSICION:
+### Dividir un problema en unos mas pequeñitos
+
+
+class Automovil:
+
+    def __init__(self, modelo, marca, color):
+        self.modelo = modelo
+        self.marca = marca
+        self.color = color
+        self._estado = 'en_reposo' #private
+        #self._motor = None #private
+        self._motor = Motor(cilindros=4)
+    
+    def acelerar(self, tipo='despacio'):
+        if tipo == 'rapida':
+            self._motor.inyecta_gasolina(10)
+        else:
+            self._motor.inyecta_gasolina(3)
+        self._estado = 'movimiento'
+
+#modelar el motor
+class Motor:
+    def __init__(self, cilindros, tipo='gasolina'):
+        self.cilindros = cilindros
+        self.tipo = tipo
+        self._temperatura = 0
+
+    def inyecta_gasolina(self, cantidad):
+        pass
